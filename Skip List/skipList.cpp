921d07b824805b28b076c4ea326c51c3d21082b5
GeorgeOrzanescu <george.asro@gmail.com>
2022-01-15 15:49:35 +0200

    Find works ok , problems with insert

--- Skip List/skipList.cpp
+++ Skip List/skipList.cpp
@@ -14,28 +14,28 @@ public:
     Node()
     {
         data = 0;
-        next.push_back(nullptr);
-        prev.push_back(nullptr);
+        next.resize(3);
+        prev.resize(3);
     }
 
     Node(int _data)
     {
         data = _data;
-        next.push_back(nullptr);
-        prev.push_back(nullptr);
+        next.resize(3);
+        prev.resize(3);
     }
 
-    // Node(const Node &n)
-    // {
-    //     data = n.data;
-    //     next = n.next;
-    //     prev = n.prev;
-    // }
+    Node(const Node &n)
+    {
+        data = n.data;
+        next = n.next;
+        prev = n.prev;
+    }
 
     void linkNodes(Node *_prev, Node *_next, int layer)
     {
-        _prev->next.emplace(_prev->next.begin() + layer, _next);
-        _next->prev.emplace(_next->prev.begin() + layer, _prev);
+        _prev->next.insert(_prev->next.begin() + layer, _next);
+        _next->prev.insert(_next->prev.begin() + layer, _prev);
     }
 
     void printNode()
@@ -46,21 +46,80 @@ public:
         for (int i = 0; i < next.size(); i++)
         {
             if (next[i] != nullptr)
-                cout << next[i]->data << " \n";
+                cout << "Layer" << i << next[i]->data << " \n";
             else
-                cout << "NULL ";
+                cout << "Layer" << i << "NULL ";
         }
         cout << endl;
         for (int i = 0; i < prev.size(); i++)
         {
             if (prev[i] != nullptr)
-                cout << prev[i]->data << "\n ";
+                cout << "Layer" << i << prev[i]->data << "\n ";
             else
-                cout << "NULL ";
+                cout << "Layer" << i << "NULL ";
         }
     }
-};
 
+    bool findNode(Node *head, int data)
+    {
+        Node *current = head;
+        int layer = head->next.size() - 1;
+        while (layer >= 0)
+        {
+            if (current->data == data)
+            {
+                return true;
+            }
+            else if (current->next[layer] == NULL || current->next[layer]->data > data)
+            {
+                layer = layer - 1;
+            }
+            else
+                current = current->next[layer];
+        }
+        return false;
+    }
+
+    void insertNode(Node *head, int data)
+    {
+        Node *current = head;
+        int layer = head->next.size() - 1;
+
+        while (layer >= 0)
+        {
+            if (current->next[layer] == NULL || current->next[layer]->data > data)
+            {
+                layer = layer - 1;
+            }
+            else if (current->next[layer]->data > data)
+            {
+                break;
+            }
+            else
+            {
+                current = current->next[layer];
+            }
+        }
+
+        Node *newnode = new Node(data);
+        newnode->linkNodes(current, newnode, 0);
+
+        // find suitable forward node to link to
+        int suitableLayer = 0;
+        for (int i = 0; i < newnode->next.size(); i++)
+        {
+            if (current->next[i] == NULL)
+            {
+                continue;
+            }
+            else
+            {
+                suitableLayer = i;
+            }
+        }
+        newnode->linkNodes(newnode, current->next[suitableLayer], 1);
+    }
+};
 int main()
 {
 
@@ -68,11 +127,20 @@ int main()
     Node first(1);
     Node second(2);
     Node third(3);
+    Node seventh(7);
 
-    head.linkNodes(&head, &first, -1);
+    head.linkNodes(&head, &first, 0);
+    head.linkNodes(&first, &second, 0);
     head.linkNodes(&head, &third, 1);
+    head.linkNodes(&third, &seventh, 1);
 
-    head.printNode();
-    first.printNode();
+    // head.printNode();
+    // first.printNode();
+    // second.printNode();
+    // third.printNode();
+    // seventh.printNode();
+
+    head.insertNode(&head, 4);
     third.printNode();
+    seventh.printNode();
 }
